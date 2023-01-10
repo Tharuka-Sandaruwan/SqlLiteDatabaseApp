0bb9d5e2a355c1245cbbbbb0d91177e29be4edf1
Tharuka Sandaruwan <ts2000614@gmail.com>
2023-01-10 14:55:11 +0530

    insertdata created

--- app/src/main/java/com/example/sqllitedatabaseapp/MainActivity.java
+++ app/src/main/java/com/example/sqllitedatabaseapp/MainActivity.java
@@ -41,4 +41,18 @@ public class MainActivity extends AppCompatActivity {
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerId.setAdapter(dataAdapter);
     }
+
+    public void insertData(View view){
+        EditText fName=(EditText)findViewById(R.id.editTextFName);
+        EditText surname=(EditText)findViewById(R.id.editTextSurname);
+        EditText marks=(EditText)findViewById(R.id.editTextMarks);
+        boolean executed= databaseHelper.insertToDb(fName.getText().toString(),surname.getText().toString(),Integer.parseInt(marks.getText().toString()));
+        if(executed){
+            Toast.makeText(this,"Data Inserted",Toast.LENGTH_LONG).show();
+            viewAllIds();
+        }
+        else{
+            Toast.makeText(this,"Something Went Wrong",Toast.LENGTH_LONG).show();
+        }
+    }
 }
